6cea812d4c5878f926b61f3193ba85e9365ae900
Arielle Foldoe <afoldoe@gmail.com>
2016-03-22 23:38:47 -0700

    added objects and html intro

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cookie-stand
+
+Added individual objects for each cookie shop; gave three properties (min number of customers/hr, max number of customers/hr, method calculating avg # of customers/hr).
---
+++ main.js
@@ -0,0 +1,40 @@
+var pioneerSq = {
+  minCust: 17,
+  maxCust: 88,
+  aveCookieSale: 5.2,
+  custPerHour: function() {
+    Math.floor(Math.random() * 10) + 1;
+  }
+};
+var pdxAirport = {
+  minCust: 6,
+  maxCust: 24,
+  aveCookieSale: 1.2,
+  custPerHour: function() {
+    Math.floor(Math.random() * 10) + 1;
+  }
+};
+var washSq = {
+  minCust: 11,
+  maxCust: 38,
+  aveCookieSale: 1.9,
+  custPerHour: function() {
+    Math.floor(Math.random() * 10) + 1;
+  }
+};
+var sellwood = {
+  minCust: 20,
+  maxCust: 48,
+  aveCookieSale: 3,3,
+  custPerHour: function() {
+    Math.floor(Math.random() * 10) + 1;
+  }
+};
+var pearl = {
+  minCust: 3,
+  maxCust: 24,
+  aveCookieSale: 2.6,
+  custPerHour: function() {
+    Math.floor(Math.random() * 10) + 1;
+  }
+};
