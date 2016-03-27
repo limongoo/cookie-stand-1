57bd5610a26ee546710b129f5a64324ab09280fa
Arielle Foldoe <afoldoe@gmail.com>
2016-03-27 13:50:02 -0700

    added comments to js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var pioneerSq = {
   totalCookies: 0,
   storeID: "pioneerSq",
   custPerHour: function() {
-    return Math.floor(Math.random() * 10) + 1;
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
   }
 };
 var pdxAirport = {
@@ -18,7 +18,7 @@ var pdxAirport = {
   totalCookies: 0,
   storeID: "pdxAirport",
   custPerHour: function() {
-    return Math.floor(Math.random() * 10) + 1;
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
   }
 };
 var washSq = {
@@ -29,7 +29,7 @@ var washSq = {
   totalCookies: 0,
   storeID: "washSq",
   custPerHour: function() {
-    return Math.floor(Math.random() * 10) + 1;
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
   }
 };
 var sellwood = {
@@ -40,7 +40,7 @@ var sellwood = {
   totalCookies: 0,
   storeID: "sellwood",
   custPerHour: function() {
-    return Math.floor(Math.random() * 10) + 1;
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
   }
 };
 var pearl = {
@@ -51,24 +51,25 @@ var pearl = {
   totalCookies: 0,
   storeID: "pearl",
   custPerHour: function() {
-    return Math.floor(Math.random() * 10) + 1;
+    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
   }
 };
 
 //stores each object into one array
 var stores = [pioneerSq, pdxAirport, washSq, sellwood, pearl];
 
-//each of the store is open
+//each hour the store is open
 var hours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
+//calculates # of cookies per hour sold per store
 for(storeIndex = 0; storeIndex < stores.length; storeIndex++) {
   for(hoursIndex = 0; hoursIndex < hours.length; hoursIndex++) {
     var store = stores[storeIndex];
     var cookies = Math.round(store.avgCookieSale * store.custPerHour());
-    store.cookies.push(cookies);
+    store.cookies.push(cookies); //calculates a random # of cookies sold/hr
     store.totalCookies += cookies;
-    document.getElementById(store.storeID).innerHTML += "<li>" + hours[hoursIndex] + ": " + cookies + " cookies sold. </li>"
+    document.getElementById(store.storeID).innerHTML += "<li>" + hours[hoursIndex] + ": " + cookies + " cookies sold. </li>" //prints the hour and cookies sold in that hr
 
   }
-  document.getElementById(store.storeID).innerHTML += "<li>Total: " + store.totalCookies + " sold today!"
+  document.getElementById(store.storeID).innerHTML += "<li>Total: " + store.totalCookies + " sold today!" //prints total cookies sold daily
 }
