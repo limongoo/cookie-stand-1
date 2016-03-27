744fa5d1d97194e75a39dc29d6222e72ac6b3816
Arielle Foldoe <afoldoe@gmail.com>
2016-03-26 19:51:59 -0700

    updated js and html

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cookie-stand
 
 Added individual objects for each cookie shop; gave three properties (min number of customers/hr, max number of customers/hr, method calculating avg # of customers/hr).
+
+Added a for loop for each object to calculate the cookies sold per hour based on avgCookieSale and a random number of custPerHour. printed the # of cookies sold per hour for each store to the page as an unordered list.
--- main.js
+++ main.js
@@ -1,44 +1,82 @@
-var stores = [pioneerSq, pdxAirport, washSq, sellwood, pearl];
-function cookiesPerHour {
-    stores[i].custPerHour * stores[i].aveCookieSale;
-}
+//five objects representing each store
 var pioneerSq = {
   minCust: 17,
   maxCust: 88,
-  aveCookieSale: 5.2,
+  avgCookieSale: 5.2,
   custPerHour: function() {
-    Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
   }
 };
 var pdxAirport = {
   minCust: 6,
   maxCust: 24,
-  aveCookieSale: 1.2,
+  avgCookieSale: 1.2,
   custPerHour: function() {
-    Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
   }
 };
 var washSq = {
   minCust: 11,
   maxCust: 38,
-  aveCookieSale: 1.9,
+  avgCookieSale: 1.9,
   custPerHour: function() {
-    Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
   }
 };
 var sellwood = {
   minCust: 20,
   maxCust: 48,
-  aveCookieSale: 3.3,
+  avgCookieSale: 3.3,
   custPerHour: function() {
-    Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
   }
 };
 var pearl = {
   minCust: 3,
   maxCust: 24,
-  aveCookieSale: 2.6,
+  avgCookieSale: 2.6,
   custPerHour: function() {
-    Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
   }
 };
+var stores = [pioneerSq, pdxAirport, washSq, sellwood, pearl];
+var pearlCookiesPerHour = [];
+var pioneerSqCookiesPerHour = [];
+var pdxAirportCookiesPerHour = [];
+var washSqCookiesPerHour = [];
+var sellwoodCookiesPerHour = [];
+
+//calculates cookies purchased per hour for pearl
+for(i=0;i<8;i++) {
+  var pearlCookies = Math.round(pearl.avgCookieSale * pearl.custPerHour());
+  pearlCookiesPerHour.push(pearlCookies);
+  document.getElementById("pearl").innerHTML += "<li>" + i + "am: " + pearlCookies + "</li>";
+}
+
+//calculates cookies purchased per hour for pioneerSq
+for(i=0;i<8;i++) {
+  var pioneerSqCookies = Math.round(pioneerSq.avgCookieSale * pioneerSq.custPerHour());
+  pioneerSqCookiesPerHour.push(pioneerSqCookies);
+  document.getElementById("pioneerSq").innerHTML += "<li>" + i + "am: " + pioneerSqCookies + "</li>";
+}
+
+//calculates cookies purchased per hour for sellwood
+for(i=0;i<8;i++) {
+  var sellwoodCookies = Math.round(sellwood.avgCookieSale * sellwood.custPerHour());
+  sellwoodCookiesPerHour.push(sellwoodCookies);
+  document.getElementById("sellwood").innerHTML += "<li>" + i + "am: " + sellwoodCookies + "</li>";
+}
+
+//calculates cookies purchased per hour for pdxAirport
+for(i=0;i<8;i++) {
+  var pdxAirportCookies = Math.round(pdxAirport.avgCookieSale * pdxAirport.custPerHour());
+  pdxAirportCookiesPerHour.push(pdxAirportCookies);
+  document.getElementById("pdxAirport").innerHTML += "<li>" + i + "am: " + pdxAirportCookies + "</li>";
+}
+
+//calculates cookies purchased per hour for washSq
+for(i=0;i<8;i++) {
+  var washSqCookies = Math.round(washSq.avgCookieSale * washSq.custPerHour());
+  washSqCookiesPerHour.push(washSqCookies);
+  document.getElementById("washSq").innerHTML += "<li>" + i + "am: " + washSqCookies + "</li>";
+}
