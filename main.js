bc83a5b56b81c3f84b6d210dc1f203e18312fb1e
Ivan Limongan <ivan.limongan@gmail.com>
2016-03-31 20:43:26 -0700

    table working

--- main.js
+++ main.js
@@ -8,32 +8,41 @@ var Store = function(minCust, maxCust, avgCookieSale, cookies, totalCookies, sto
   this.storeID = storeID;
   this.custPerHour = function() {
   return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+  }
 };
 //five objects representing each store
-var pioneerSq = new Store(17, 88, 5.2, [], 0, "pioneerSq");
-var pdxAirport = new Store(6, 24, 2.2, [], 0, "pdxAirport");
-var washSq = new Store(11, 38, 1.9, [], 0, "washSq");
-var sellwood = new Store(20, 48, 3.3, [], 0, "sellwood");
-var pearl = new Store(3, 24, 2.6, [], 0, "pearl");
+var pioneerSq = new Store(17, 88, 5.2, new Array(), 0, "pioneerSq");
+var pdxAirport = new Store(6, 24, 2.2, new Array(), 0, "pdxAirport");
+var washSq = new Store(11, 38, 1.9, new Array(), 0, "washSq");
+var sellwood = new Store(20, 48, 3.3, new Array(), 0, "sellwood");
+var pearl = new Store(3, 24, 2.6, new Array(), 0, "pearl");
 
 //stores each object into one array
 var stores = [pioneerSq, pdxAirport, washSq, sellwood, pearl];
 
 //each hour the store is open
 var hours = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var table = document.getElementById('tableBody');
+
+
 
 //calculates # of cookies per hour sold per store
 for(storeIndex = 0; storeIndex < stores.length; storeIndex++) {
+  var store = stores[storeIndex];
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  cell1.innerHTML = store.storeID;
+
   for(hoursIndex = 0; hoursIndex < hours.length; hoursIndex++) {
-    var store = stores[storeIndex];
-    var cookies = Math.round(store.avgCookieSale * store.custPerHour());
-    store.cookies.push(cookies); //calculates a random # of cookies sold/hr
-    store.totalCookies += cookies;
-    var cell = document.getElementById("tableCell");
-    var list = document.createElement("ul");
-    list.setAttribute("id", store.storeID);
-    cell.appendChild(list);
-    document.getElementById(store.storeID).innerHTML += "<li>" + hours[hoursIndex] + ": " + cookies + " cookies sold. </li>";//prints list of cookies sold
+
+    var cookiesSold = Math.round(store.avgCookieSale * store.custPerHour());
+    store.cookies.push(cookiesSold); //calculates a random # of cookies sold/hr
+    store.totalCookies += cookiesSold;
+    cell2 = row.insertCell(1);
+    cell2.innerHTML = cookiesSold;
+
   }
-  document.getElementById(store.storeID).innerHTML += "<li>Total: " + store.totalCookies + " sold today!";
+  cell11 = row.insertCell(11);
+  cell11.innerHTML = store.totalCookies;
+  // document.getElementById(store.storeID).innerHTML += "<li>Total: " + store.totalCookies + " sold today!";
 } //prints total cookies sold daily
