d99adf7fa4023a96a3d052b3dd8947a3dbd2ad79
Arielle Foldoe <afoldoe@gmail.com>
2016-03-31 17:03:11 -0700

    class7

--- main.js
+++ main.js
@@ -1,59 +1,20 @@
+
+var Store = function(minCust, maxCust, avgCookieSale, cookies, totalCookies, storeID) {
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSale = avgCookieSale;
+  this.cookies = cookies;
+  this.totalCookies = totalCookies;
+  this.storeID = storeID;
+  this.custPerHour = function() {
+  return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+};
 //five objects representing each store
-var pioneerSq = {
-  minCust: 17,
-  maxCust: 88,
-  avgCookieSale: 5.2,
-  cookies: [],
-  totalCookies: 0,
-  storeID: "pioneerSq",
-  custPerHour: function() {
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-  }
-};
-var pdxAirport = {
-  minCust: 6,
-  maxCust: 24,
-  avgCookieSale: 1.2,
-  cookies: [],
-  totalCookies: 0,
-  storeID: "pdxAirport",
-  custPerHour: function() {
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-  }
-};
-var washSq = {
-  minCust: 11,
-  maxCust: 38,
-  avgCookieSale: 1.9,
-  cookies: [],
-  totalCookies: 0,
-  storeID: "washSq",
-  custPerHour: function() {
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-  }
-};
-var sellwood = {
-  minCust: 20,
-  maxCust: 48,
-  avgCookieSale: 3.3,
-  cookies: [],
-  totalCookies: 0,
-  storeID: "sellwood",
-  custPerHour: function() {
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-  }
-};
-var pearl = {
-  minCust: 3,
-  maxCust: 24,
-  avgCookieSale: 2.6,
-  cookies: [],
-  totalCookies: 0,
-  storeID: "pearl",
-  custPerHour: function() {
-    return Math.random() * (this.maxCust - this.minCust) + this.minCust;
-  }
-};
+var pioneerSq = new Store(17, 88, 5.2, [], 0, "pioneerSq");
+var pdxAirport = new Store(6, 24, 2.2, [], 0, "pdxAirport");
+var washSq = new Store(11, 38, 1.9, [], 0, "washSq");
+var sellwood = new Store(20, 48, 3.3, [], 0, "sellwood");
+var pearl = new Store(3, 24, 2.6, [], 0, "pearl");
 
 //stores each object into one array
 var stores = [pioneerSq, pdxAirport, washSq, sellwood, pearl];
@@ -72,8 +33,7 @@ for(storeIndex = 0; storeIndex < stores.length; storeIndex++) {
     var list = document.createElement("ul");
     list.setAttribute("id", store.storeID);
     cell.appendChild(list);
-    document.getElementById(store.storeID).innerHTML += "<li>" + hours[hoursIndex] + ": " + cookies + " cookies sold. </li>";
-
+    document.getElementById(store.storeID).innerHTML += "<li>" + hours[hoursIndex] + ": " + cookies + " cookies sold. </li>";//prints list of cookies sold
   }
-  document.getElementById(store.storeID).innerHTML += "<li>Total: " + store.totalCookies + " sold today!" //prints total cookies sold daily
-}
+  document.getElementById(store.storeID).innerHTML += "<li>Total: " + store.totalCookies + " sold today!";
+} //prints total cookies sold daily
